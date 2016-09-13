9a0cf68cf7b0a7679dde535859245d840f82112d
leedavi <dcl@nevoweb.com>
2016-09-13 07:45:15 +0200

    add missing logwriter

---
+++ Components/LogWriter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Nevoweb.DNN.NBrightBuyMigrate.Components
+{
+
+    public class LogWriter
+    {
+        private string m_exePath = string.Empty;
+        public LogWriter(string logMessage)
+        {
+            LogWrite(logMessage);
+        }
+        public void LogWrite(string logMessage)
+        {
+            var controlMapPath = HttpContext.Current.Server.MapPath("/DesktopModules/NBright/NBrightBuyMigrate");
+            try
+            {
+                using (StreamWriter w = File.AppendText(controlMapPath + "\\" + "log.txt"))
+                {
+                    Log(logMessage, w);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        public void Log(string logMessage, TextWriter txtWriter)
+        {
+            try
+            {
+                txtWriter.Write("\r\nLog Entry : ");
+                txtWriter.WriteLine("  :{0}", logMessage);
+                txtWriter.WriteLine("-------------------------------");
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+
+}
